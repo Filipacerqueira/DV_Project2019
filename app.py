b66254b20a7c65ce4dc8b1cb738d0fafaac00a3b
m20190269 <55530339+m20190269@users.noreply.github.com>
2020-01-16 17:02:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ del all_data, countries_complete, data_2018, data_2017, data_2016, city, country
 country_options = [dict(label=country, value=country) for country in df_opportunity.iloc[:, 0].unique()]
 
 app = dash.Dash(__name__)
-server=app.server()
+server=app.server
 
 variable_options = [dict(label=variable, value=variable) for variable in df.columns[1:]]
 
